34e69d787fc2041c995c7e4dc95c6ca9c7912f7e
Vũ Hoàng Long <long189b@gmail.com>
2018-11-01 10:24:54 +0700

    Edit port to 65080

--- app.js
+++ app.js
@@ -19,6 +19,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(65080, function(){
+  console.log('listening on *:65080');
 });
